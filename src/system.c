cbb3bcaa7c0d6fe12192c665facb7e4595332b91
prashanna <prashandon97@gmail.com>
2026-07-24 20:00:50 +0545

    init system.c

---
+++ src/system.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+static void printHostname(void)
+{
+  // Code to print hostname
+}
+
+static void printUser(void)
+{
+  // Code to print username
+}
+
+static void printKernel(void)
+{
+  // Code to print kernel version
+}
+
+static void printOS(void)
+{
+  // Code to print OS
+}
+
+void printSystemInfo(void)
+{
+  printHostname();
+  printUser();
+  printKernel();
+  printOS();
+}
