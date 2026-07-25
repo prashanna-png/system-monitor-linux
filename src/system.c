5dc9c3f577e1560a8d6c6ae4246cb78011b6e334
prashanna <prashandon97@gmail.com>
2026-07-25 10:16:38 +0545

    display username

--- src/system.c
+++ src/system.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pwd.h>
 
 static void printHostname(void)
 {
   char hostname[256];
   if (gethostname(hostname, sizeof(hostname)) == 0)
   {
+    // terminal command = "hostname"
     printf("Hostname : %s\n", hostname);
   }
   else
@@ -16,6 +18,21 @@ static void printHostname(void)
 
 static void printUser(void)
 {
+  // terminal command = "whoami"
+  __uid_t uid = getuid();
+
+  printf("Userid: %d\n", uid);
+
+  struct passwd *pw = getpwuid(uid);
+
+  if (pw != NULL)
+  {
+    printf("Username : %s\n", pw->pw_name);
+  }
+  else
+  {
+    printf("failed to retrieve username");
+  }
 }
 
 static void printKernel(void)
