06f91f499a5f231968c88e0742bda1a06f6e5619
prashanna <prashandon97@gmail.com>
2026-07-25 10:26:11 +0545

    Add system information module with hostname and username

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include "system.h"
+#include <stdio.h>
 
 int main(void)
 {
+  printf("==================================\n");
+  printf("\tSystem Information\n");
+  printf("==================================\n");
   printSystemInfo();
   return 0;
 }
