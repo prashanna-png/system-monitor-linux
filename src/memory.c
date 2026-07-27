60dd608e25a57293b4616f2c33d0f63741600fb7
prashanna <prashandon97@gmail.com>
2026-07-27 13:33:09 +0545

    use one function printMemoryStats()

--- src/memory.c
+++ src/memory.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-static void printTotalMemory(void)
+static void printMemoryStats(void)
 {
   FILE *fp = fopen("/proc/meminfo", "r");
 
   char line[200];
-  char target[] = "MemTotal:";
+  char target1[] = "MemTotal:";
+  char target2[] = "MemAvailable:";
+  double totalGB;
+  double availableGB;
+  double usedMemory;
+
   if (fp == NULL)
   {
     printf("unable to open file\n");
@@ -14,52 +19,32 @@ static void printTotalMemory(void)
   }
   while (fgets(line, sizeof(line), fp))
   {
-    if (strncmp(target, line, strlen(target)) == 0)
+    if (strncmp(target1, line, strlen(target1)) == 0)
     {
       int totalMem;
       char *start = strrchr(line, ':');
       sscanf(start + 1, "%d", &totalMem);
 
-      double totalGB = totalMem / (1024.0 * 1024.0);
-      printf("Total RAM\t:%.2f GB\n", totalGB);
+      totalGB = totalMem / (1024.0 * 1024.0);
+      printf("Total RAM\t: %.2f GB\n", totalGB);
     }
-  }
-  fclose(fp);
-}
-static void printAvailableMemory(void)
-{
-
-  FILE *fp = fopen("/proc/meminfo", "r");
-
-  char line[200];
-  char target[] = "MemAvailable:";
-  if (fp == NULL)
-  {
-    printf("unable to open file\n");
-    return;
-  }
-  while (fgets(line, sizeof(line), fp))
-  {
-    if (strncmp(target, line, strlen(target)) == 0)
+    if (strncmp(target2, line, strlen(target2)) == 0)
     {
       int totalAvailable;
       char *start = strrchr(line, ':');
       sscanf(start + 1, "%d", &totalAvailable);
 
-      double totalGB = totalAvailable / (1024.0 * 1024.0);
-      printf("Available\t:%.2f GB\n", totalGB);
+      availableGB = totalAvailable / (1024.0 * 1024.0);
+      printf("Available\t: %.2f GB\n", availableGB);
     }
   }
+
+  usedMemory = totalGB - availableGB;
+  printf("Used\t\t: %.2f GB\n", usedMemory);
   fclose(fp);
-}
-static void printUsedMemory(void)
-{
-  
 }
 
 void printMemoryInfo(void)
 {
-  printTotalMemory();
-  printAvailableMemory();
-  printUsedMemory();
+  printMemoryStats();
 }
